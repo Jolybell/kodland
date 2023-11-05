77fdbc92c96f5c43e542d47912918da4dbbd95b6
Jolybell <ihelda735@gmail.com>
2023-11-05 12:54:43 +0300

    s ს

---
+++ test.py
@@ -0,0 +1,11 @@
+# Так можно прочитать весь файл
+f = open('text.txt', 'w', encoding='utf-8')
+text = f.write("'" текстовыми файлами:\n"'")
+print(text)
+f.close()
+
+"'"\
+"r " - "read"
+'w ' - 'write'
+'a ' - 'append'
+"'"
